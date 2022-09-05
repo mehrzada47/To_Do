866e7f63d7a331b4b13290700702dc5be25b4b9c
mehrzada47 <mehrzada47@gmail.com>
2022-09-06 03:35:32 +0430

    add search object

--- To_do(js).js
+++ To_do(js).js
@@ -1,19 +1,6 @@
-let  addButton=document.querySelector("#add_button");
-let addInput=document.querySelector("#add");
-let searchButton=document.querySelector("#search_button");
-let searchInput=document.querySelector("#search");
-addButton.addEventListener('click',e=>{
-    addInput.classList.toggle("show");
-    searchInput.classList.remove("show");
-})
-
-searchButton.addEventListener("click",e=>{
-    searchInput.classList.toggle("show");
-    addInput.classList.remove("show");
-})
 
 let works=localStorage.getItem("todos");
-
+var is_exit=true;
 try{
 works=JSON.parse(works);
 works=works.length?works:null;
@@ -34,7 +21,7 @@ if (!works) {
 function todo_func(todos) {
     let groupItems=document.querySelector("#list-item-group");
     groupItems.innerHTML="";
-    works.forEach((todo,index)=>{
+    todos.forEach((todo,index)=>{
         let li=document.createElement("li");
         li.classList.add("list-group-item");
         let span=document.createElement("span");
@@ -63,3 +50,53 @@ function todo_func(todos) {
     })
 }
 todo_func(works);
+//
+// addInput.addEventListener("submit",evently=>{
+//   console.log(addInput.textContent);
+// })
+let  addButton=document.querySelector("#add_button");
+let addInput=document.querySelector("#add");
+let searchButton=document.querySelector("#search_button");
+let searchInput=document.querySelector("#search");
+addButton.addEventListener('click',e=>{
+    addInput.classList.toggle("show");
+    searchInput.classList.remove("show");
+    addInput.addEventListener('submit',event=>{
+        event.preventDefault();
+        if(addInput.add.value){
+            works.forEach(e=>{
+                if(e.content==addInput.add.value){
+                is_exit=false;
+                }
+            })
+            if (is_exit==true){
+                works.push({content:addInput.add.value,status:true});
+                localStorage.setItem("todos",JSON.stringify(works));
+                todo_func(works);
+                addInput.add.value="";
+            }else {
+                alert("this activity now is available");
+                addInput.add.value="";
+            }
+        }
+    })
+})
+
+    searchButton.addEventListener("click",e=>{
+    searchInput.classList.toggle("show");
+    addInput.classList.remove("show");
+    searchInput.addEventListener("keyup",e=>{
+        e.preventDefault();
+        if (searchInput.search.value){
+            let filtered=[];
+            works.forEach(e=>{
+                if(e.content.toLowerCase().includes(searchInput.search.value.toLowerCase())){
+                filtered.push( {content:e.content, status: true})
+                }
+            })
+            todo_func(filtered);
+        }else {
+            todo_func(works);
+        }
+    })
+    })
